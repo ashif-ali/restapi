9ae3f4f45f2b1ad2bb835fa018d6f56f561fa7e4
Sk Md Ashif Ali <36637666+ashif-ali@users.noreply.github.com>
2025-04-16 23:00:42 +0530

    completed the read expenses api

---
+++ src/main/java/in/ashifali/restapi/service/ExpenseService.java
@@ -0,0 +1,18 @@
+package in.ashifali.restapi.service;
+
+import in.ashifali.restapi.dto.ExpenseDTO;
+
+import java.util.List;
+
+/**
+ * Service interface for Expense module
+ * @author Ashif Ali
+ * */
+public interface ExpenseService {
+
+    /**
+     * It will fetch the expenses from database
+     * @return list
+     * */
+    List<ExpenseDTO> getAllExpenses();
+}
